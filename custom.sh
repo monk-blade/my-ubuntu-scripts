0d67205f5ec7a8fd627603ef065b483e154d6f15
Arpan Chavda <arpanchavdaeng@gmail.com>
2013-08-31 13:04:01 +0530

    moved some scripts to this folder

--- custom.sh
+++
@@ -1,7 +0,0 @@
-echo "Please run ppa.sh before running this script..."
-echo "Installing some softwares into your system..."
-sudo apt-get install pip ruby smuxi
-#install spinx,jekyll and themes
-sudo pip install sphinx sphinx_bootstrap_theme
-sudo gem install jekyll
-echo "Go to https://github.com/ryan-roemer/sphinx-bootstrap-theme for configuration and html options"
